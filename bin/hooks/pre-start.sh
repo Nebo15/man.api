44c6cece0099ebe96e1d0b548453dc08d47af253
Andrew Dryga <andrew@dryga.com>
2017-04-19 23:15:21 +0300

    Typo

--- bin/hooks/pre-start.sh
+++ bin/hooks/pre-start.sh
@@ -4,5 +4,5 @@ APP_NAME="man_api"
 
 if [ "${DB_MIGRATE}" == "true" ]; then
   echo "[WARNING] Migrating database!"
-  ./bin/$APP_NAME command "Elixir.Postboy.ReleaseTasks" migrate!
+  ./bin/$APP_NAME command "Elixir.Man.ReleaseTasks" migrate!
 fi;
