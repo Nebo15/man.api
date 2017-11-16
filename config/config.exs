2c589438911a5d12bbd3ab85dd8026c9d9eeddc6
gmile <iamexile@gmail.com>
2017-11-16 17:43:42 +0200

    Only log info and above

--- config/config.exs
+++ config/config.exs
@@ -57,8 +57,9 @@ config :man_api,
 
 # Configures Elixir's Logger
 config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
+  format: "$message\n",
+  metadata: [:request_id],
+  level: :info
 
 # This requires installing wkhtmltopdf onto developers machine
 config :pdf_generator,
